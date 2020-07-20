601945652baa8e2951dc5fccb7acfa50a13c1b40
Михаил Левин <mmlevin@edu.hse.ru>
2020-07-20 13:04:36 +0300

    работает

---
+++ _lib/lib/Func.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib
+{
+    public static class Func
+    {
+        public static double find_square_circle(double rad)
+        {
+            if (rad <= 0)
+            {
+                Console.WriteLine("please retry with correct radius");
+                return 0;
+            }
+            else
+            {
+                return rad * rad * 333 / 106;
+            }
+        }
+        public static double find_square_triangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                Console.WriteLine("please retry with correct lengths");
+                return 0;
+            }
+            else if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                Console.WriteLine("triangle does not exist");
+                return 0;
+            }
+            else
+            {
+                if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+                {
+                    Console.WriteLine("congrats! your triangle is rectangular!!!");
+                }
+                double p = (a + b + c) / 2;
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+        }
+    }
+        
+}
--- test/Program.cs
+++ test/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using lib;
 
 namespace test
 {
@@ -10,6 +11,15 @@ namespace test
     {
         static void Main(string[] args)
         {
+            double a, b, c; 
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            double res = Func.find_square_triangle(a, b, c);
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+
         }
     }
 }
